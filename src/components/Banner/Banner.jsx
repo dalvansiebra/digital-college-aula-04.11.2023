7afd1fc0b63eebdd83611fbb4e6e5e85a6402f8a
Dalvan Siebra <dalvansiebra@outlook.com>
2023-11-14 00:29:37 -0300

    iniciando o banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import BannerText from "./BannerText";
+
+export default function Banner () {
+    return (
+        <>
+            <div>
+                <BannerText/>
+            </div>
+        </>    
+    )
+}
---
+++ src/components/Banner/BannerText.jsx
@@ -0,0 +1,10 @@
+export default function BannerText () {
+    return(
+        <>
+            <h2>Hi, It's me</h2>
+            <h1>MD RIEAD MIA</h1>
+            <h2>And I'm a Frontend Developer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolorum ullam molestias autem deleniti iure earum!</p>
+        </>    
+    )
+}
